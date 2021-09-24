cdb95fc60b87db8f698c9953342194dafe478350
Antonio Cintora <tonicintora@gmail.com>
2021-09-24 21:25:44 +0200

    main

---
+++ ejercicio12/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios PHP</title>
+</head>
+<body>
+    <div class="ejercicio11">
+        <!-- Escribe un script que simule el comportamiento de lanzar una moneda al aire y muestre una
+        imagen con la cara o la cruz de la moneda.
+        Utiliza: la función rand y un elemento de imagen <img> en HTML.-->
+        <form action="." method="POST">
+            <input type="submit" value="Lanzar Moneda">
+        </form>
+        <?php
+            $moneda= $POST_["submit"];
+        ?>        
+    </div>
+    
+    
+</body>
+</html>
