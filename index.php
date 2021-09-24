8d1c71e2cec70347b54e1a9881488712824cb5d3
Antonio Cintora Romero <tonicintora@gmail.com>
2021-09-24 12:05:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios PHP</title>
+</head>
+<body>
+    <div class="ejercicio1">
+        <?php
+        /* Escribe un programa que muestre por pantalla la cadena de texto “Hola Mundo” usando la
+        instrucción echo. No se pueden utilizar variables.*/
+        echo "Hola Mundo";
+        ?>
+    </div>
+    
+</body>
+</html>
